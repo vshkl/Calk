a0a99de6d1538c0cfccf227cfca1a3599833aaaf
Pavel Vashkel <p.vashkel@datarockets.com>
2023-02-15 17:50:04 +0100

    Implement common ios calback viewmodel

---
+++ shared/src/iosMain/kotlin/co/touchlab/kampkit/CalculatorCallbackViewModel.kt
@@ -0,0 +1,20 @@
+package co.touchlab.kampkit
+
+import co.touchlab.kampkit.features.calculator.InputAction
+import co.touchlab.kampkit.models.CalculatorViewModel
+import co.touchlab.kampkit.models.CallbackViewModel
+import co.touchlab.kermit.Logger
+
+@Suppress("Unused") // Members are called from Swift
+class CalculatorCallbackViewModel(
+    log: Logger
+) : CallbackViewModel() {
+
+    override val viewModel = CalculatorViewModel(log)
+
+    val calculatorState = viewModel.calculatorState.asCallbacks()
+
+    fun modifyInput(inputAction: InputAction) {
+        viewModel.modifyInput(inputAction)
+    }
+}
